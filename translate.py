2141c56422bf5b964bb50bb439cc57d3ac6d3edf
Jinjii <67595078+Omg-A@users.noreply.github.com>
2022-06-30 14:44:59 +0300

    Add files via upload

--- translate.py
+++ translate.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import ttk
-#from googletrans import Translator
+from googletrans import Translator
 
 root = Tk()
 root.title("Language Translater App")
-root.geometry("700x400")
+root.geometry("1000x400")
 root.configure(background="#59b7ff")
 
 label_title = Label(root, text="Language Translater App", font=("Verdana", 20), fg="white", bg="#59b7ff")
@@ -13,7 +13,10 @@ label_title.place(relx=0.5, rely=0.1, anchor=CENTER)
 label_input = Label(root, text="Enter Text", font=("Arial", 10, "bold"), bg="#59b7ff")
 label_input.place(relx=0.06, rely=0.35, anchor=CENTER)
 
-text_input = Text(root, height=10, width=50)#, wrap=WORD)
-text_input.place(relx=0.3, rely=0.6, anchor=CENTER)
+text_input = Text(root, height=10, width=50, wrap=WORD)
+text_input.place(relx=0.23, rely=0.6, anchor=CENTER)
+
+language = list(LANGUAGES.values())
+print(language)
 
 root.mainloop()
